32ae3129d2382eb49398b7aa082c029601a7bc84
Renata Osborne <renata.t.osborne@gmail.com>
2021-02-15 14:52:10 -0600

    adding files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from "react";
+import Recipe from './Recipe';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = '680ca988';
+  const APP_KEY = '1ee3c59cc34f749ff1c016ff6a3a2cb3';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+ 
+
+  useEffect( () =>{
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
+    
+    <div className="App">
+      <h1>DINNER IDEAS</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({ title,  image, ingredients}) => {
+    return (
+        <div className={style.recipe} >
+            <h1>{title}</h1>
+            <ul>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
+            <img className={style.image} src={image} />
+        </div>
+    )
+}
+
+export default Recipe;
